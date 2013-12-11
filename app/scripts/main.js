5f0fc92298b9f5eb8e4a7a578c894bbefdaebe57
rnbwd <dwisner6@gmail.com>
2013-12-11 12:14:58 -0800

    0.1

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global greyspotL, $*/
+
+
+window.greyspotL = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    greyspotL.init();
+});
